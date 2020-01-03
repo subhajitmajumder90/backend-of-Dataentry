e3eb8065595b05f4f6f390e2c622c399e3e5f370
subhajitmajumder90 <58719580+subhajitmajumder90@users.noreply.github.com>
2020-01-03 19:30:39 +0530

    3/1/2020

--- index.js
+++ index.js
@@ -33,8 +33,10 @@ app.listen(port, () => console.log(`Listening on port ${port}..`));
 //Get all users
 app.get('/getuser', (req, res) => {
     mysqlcon.query('SELECT * FROM user', (err, rows, fields) => {
-        if (!err)
-            res.send(rows);
+        if (!err){
+            res.json({data:rows}).status(200);
+            console.log(rows);
+        }
         else
             console.log(err);
     })
@@ -56,10 +58,69 @@ app.post('/Insertdata', (req, res) => {
     mysqlcon.query("INSERT INTO `user` ( `fname`, `lname`) VALUES (?,?)",[req.body.fname,req.body.lname], (err, rows, fields) => {
         if (!err)
             {
-                res.send("data inserted ");
+                res.json("inserted");
                 console.log("data inserted ");
             }
         else
             console.log(err);
     })
+});
+//fetch data id wise for update
+app.get('/fetch/:Id', (req,res)=>{
+    mysqlcon.query("select * from user where Id = ?",[req.params.Id], (err, rows, fields)=>{
+        if(err){
+            res.json("Error");
+        }
+        else{
+            data=rows[0];
+            res.json({stat:"Success", data}).status(200);
+            //res.json(rows[0]);
+            console.log(rows);
+        }
+    });
+
+});
+//update with id wise
+app.post('/update', (req,res)=>{
+    console.log(req.body);
+    mysqlcon.query("update user set fname = ?, lname = ? where Id = ?",[req.body.fname,req.body.lname,req.body.Id], (err, rows, fields)=>{
+        if(err){
+            res.json("Error");
+        }
+        else{
+            res.json("Success");
+        }
+    });
+});
+
+//Delete an employees
+app.get('/delete/:id', (req,res)=>{
+
+    mysqlcon.query("delete from user where Id = ?",[req.params.id], (err, rows, fields)=>{
+        if(err){
+            res.json("Error")
+            console.log('Error');
+        }
+        else{
+            res.json("Success")
+            console.log('Success');
+        }
+    });
+});
+
+
+//Delete an employee by post method
+
+app.post('/deletebypost',(req,res)=>{
+console.log(req.body);
+    mysqlcon.query("delete from user where Id = ?",[req.body.id],(err,rows,fields)=>{
+        if(err){
+            res.json(err)
+        }
+        else{
+            res.json("Success")
+        }
+    });
+
+
 });
